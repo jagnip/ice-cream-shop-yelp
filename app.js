3b4e1f430fbfff526005268698c84b93470e1411
Jagoda Gniadek Probulska <jagoda.gniadek.probulska@gmail.com>
2018-07-30 13:03:29 +0000

    Add new route

--- app.js
+++ app.js
@@ -10,27 +10,11 @@ app.set("view engine", "ejs");
 //SCHEMA SETUP
 var shopSchema = new mongoose.Schema( {
     name: String,
-    image: String
+    image: String,
+    description: String,
 });
 
 var Shop = mongoose.model("Shop", shopSchema);
-
-// Shop.create( {
-//     name: "GoodLood", 
-//     image: "https://images.unsplash.com/photo-1497034825429-c343d7c6a68f?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=fd67cdbdb7f3199afc745d4cdb4d10b1&auto=format&fit=crop&w=500&q=60"},
-//     (err, shop) => {
-//         if(err) {
-//             console.log(err);
-//         } else {
-//             console.log(shop);
-//         }
-//     });
-
-//  var shops = [
-//         {name: "GoodLood", image: "https://pixabay.com/get/eb37b0062df3053ed1584d05fb1d4e97e07ee3d21cac104496f2c478afeab7be_340.jpg"},
-//         {name: "Algida", image: "https://pixabay.com/get/eb37b2062bf3063ed1584d05fb1d4e97e07ee3d21cac104496f2c478afeab7be_340.jpg"},
-//         {name: "Lody na Starowiślnej", image: "https://pixabay.com/get/e832b9092bf5003ed1584d05fb1d4e97e07ee3d21cac104496f2c478afeab7be_340.jpg"},
-//         ];
 
 app.get("/", (req, res) => {
     res.render("landing");
@@ -38,7 +22,7 @@ app.get("/", (req, res) => {
 
 app.get("/shops", (req, res) => {
     
-    Shop.find({},(err, shops) => {
+    Shop.find({}, (err, shops) => {
         if(err) {
             console.log(err);
         } else {
@@ -54,10 +38,21 @@ app.get("/shops/new", (req, res) => {
 app.post("/shops", (req, res) => {
     var name = req.body.name;
     var image = req.body.image;
+    var description = req.body.shopdescription;
+    var newShop = {name: name, image: image, description: description};
     
-    shops.push({name: name, image: image});
-    res.redirect("/shops");
+    Shop.create(newShop, (err, shop) => {
+        if(err) {
+            console.log(err);
+        } else {
+            res.redirect("/shops");
+        }
+    });
 });
+
+app.get("/shops/:id", (req, res) => {
+    res.send("THIS WILL BE THE SHOW PAGE ONE DAY")
+})
 
 app.listen(process.env.PORT, process.env.IP, () => {
     console.log("Server is listening!");
