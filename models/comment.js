a81bab4db4e437ac691accbcb9c37bfe631eabc8
Jagoda Gniadek Probulska <jagoda.gniadek.probulska@gmail.com>
2018-08-01 11:19:38 +0000

    Change in comment model

--- models/comment.js
+++ models/comment.js
@@ -3,7 +3,6 @@ var mongoose = require("mongoose");
 var commentSchema = new mongoose.Schema( {
     text: String,
     author: String,
-    description: String,
 });
 
 module.exports = mongoose.model("Comment", commentSchema);
