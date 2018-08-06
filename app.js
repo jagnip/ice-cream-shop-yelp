df5987438fb9bac7a9d035fb98d9078304eb5c84
Jagoda Gniadek Probulska <jagoda.gniadek.probulska@gmail.com>
2018-08-06 10:53:18 +0000

    Add passport configuration

--- app.js
+++ app.js
@@ -17,6 +17,13 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname +"/public"));
 
+//PASSPORT CONFIGURATION
+app.use(require("express-session")({
+    secret: "Phrase to secret setup...",
+    resave: false,
+    saveUnitialized: false
+}));
+
 app.get("/", (req, res) => {
     res.render("landing");
 });
