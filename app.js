689e07a3dc5a026d03846c8c25b67691dc70d792
Jagoda Gniadek Probulska <jagoda.gniadek.probulska@gmail.com>
2018-08-06 13:26:51 +0000

    Add middleware storing information about current user

--- app.js
+++ app.js
@@ -29,6 +29,10 @@ passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
+app.use((req, res, next) => {
+    res.locals.currentUser = req.user;
+    next();
+})
 
 app.get("/", (req, res) => {
     res.render("landing");
