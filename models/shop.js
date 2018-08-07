4d8f1a6173ec6f72d000114926b789b4519fb046
Jagoda Gniadek Probulska <jagoda.gniadek.probulska@gmail.com>
2018-08-07 11:54:31 +0000

    Add author field to shop model

--- models/shop.js
+++ models/shop.js
@@ -4,6 +4,13 @@ var shopSchema = new mongoose.Schema( {
     name: String,
     image: String,
     description: String,
+    author: {
+        id: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User"
+        },
+         username: String
+    },
     comments: [
         {
             type: mongoose.Schema.Types.ObjectId,
