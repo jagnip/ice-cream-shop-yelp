62d9c63c85f8d57c11eb1e779d2adfe88e1a1a2c
Jagoda Gniadek Probulska <jagoda.gniadek.probulska@gmail.com>
2018-08-07 11:47:21 +0000

    Change id field in comment model

--- models/comment.js
+++ models/comment.js
@@ -3,10 +3,12 @@ var mongoose = require("mongoose");
 var commentSchema = new mongoose.Schema( {
     text: String,
     author: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "User"
-    },
-        username: String
+        id: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User"
+        },
+         username: String
+    }
 });
 
 module.exports = mongoose.model("Comment", commentSchema);
