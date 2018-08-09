c73591c7eacb9671f5c1254fa117c109c5c594f8
Jagoda Gniadek Probulska <jagoda.gniadek.probulska@gmail.com>
2018-08-09 09:35:07 +0000

    Add checkCommentOwnership function

--- routes/comments.js
+++ routes/comments.js
@@ -63,4 +63,22 @@ function isLoggedIn(req, res, next) {
     res.redirect("/login");
 }
 
+function checkCommentOwnership(req, res, next) {
+     if(req.isAuthenticated()) {
+        Comment.findById(req.params.comment_id, function(err, foundComment) {
+        if(err) {
+            res.redirect("back");
+        } else {
+            if(foundComment.author.id.equals(req.user._id)) {
+                next();
+            } else {
+                res.redirect("back");
+            }
+        }
+        });
+    } else {
+        res.redirect("back");
+    }
+}
+
 module.exports = router;
