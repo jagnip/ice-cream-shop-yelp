10d9d4f89ae73a7dd17e8b5d33d04cb937e76be5
Jagoda Gniadek Probulska <jagoda.gniadek.probulska@gmail.com>
2018-08-09 11:21:32 +0000

    Add flash dependency

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var bodyParser = require("body-parser");
 var mongoose = require("mongoose");
 var passport = require("passport");
 var LocalStrategy = require("passport-local");
+var flash = require("connect-flash");
 var User = require("./models/user");
 var seedDB = require("./seeds");
 var methodOverride = require("method-override");
@@ -19,6 +20,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname +"/public"));
 app.use(methodOverride("_method"));
+app.use(flash());
 
 //PASSPORT CONFIGURATION
 app.use(require("express-session")({
